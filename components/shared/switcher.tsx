d722106e8d74c794128ec8a5ddd7965299e304ff
Amir_Louati25 <ameer.louati@gmail.com>
2025-04-12 22:17:57 +0100

    Refactor ToggleSwitch component to manage internal state and improve accessibility

--- components/shared/switcher.tsx
+++ components/shared/switcher.tsx
@@ -1,28 +1,42 @@
 "use client";
+
+import { useState } from "react";
+
 const ToggleSwitch = ({
   label = "Live",
-  enabled = true,
-  onToggle,
+  disabled = false,
 }: {
   label?: string;
-  enabled?: boolean;
-  onToggle?: (enabled: boolean) => void;
+  disabled?: boolean;
 }) => {
+  const [enabled, setEnabled] = useState(false);
+  const onToggle = () => {
+    if (!disabled) {
+      setEnabled((prevState) => !prevState);
+    }
+  };
+
   return (
     <div className="flex items-center space-x-2">
       <button
-        onClick={() => onToggle?.(!enabled)}
-        className={`relative w-[39px] h-[22px] transition-colors duration-300 focus:outline-none rounded-full ${
-          enabled ? "bg-[#6FCF97]/20" : "bg-gray-300"
-        }`}
+        onClick={onToggle}
+        disabled={disabled}
+        className={`relative w-[39px] h-[22px] rounded-full transition-colors duration-300 focus:outline-none
+          ${enabled ? "bg-[#6FCF97]/20" : "bg-gray-300"}
+          ${disabled ? "cursor-not-allowed opacity-50" : "hover:opacity-90"}`}
       >
         <span
-          className={`absolute top-0  h-[22px] w-[22px] rounded-full bg-[#6FCF97] transition-transform duration-300 transform ${
-            enabled ? "left-full -translate-x-full" : "left-0"
-          }`}
+          className={`absolute top-0 h-[22px] w-[22px] rounded-full transition-transform duration-300 transform
+            ${enabled ? "bg-[#6FCF97]" : "bg-white"}
+            ${enabled ? "right-0" : "left-0"}
+            ${disabled ? "opacity-70" : ""}`}
         />
       </button>
-      <span className="text-[#828282] leading-[1.43] tracking-[0.04em]">
+      <span
+        className={`text-[#828282] leading-[1.43] tracking-[0.04em] ${
+          disabled ? "opacity-50" : ""
+        }`}
+      >
         {label}
       </span>
     </div>
