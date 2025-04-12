f1f92e8f96cf105de8d91f8b40d77c8b431d4792
Amir_Louati25 <ameer.louati@gmail.com>
2025-04-12 12:34:47 +0100

    Add LanguagesSwitcher component and integrate it into Navbar

---
+++ components/layout/header/languages-switcher.tsx
@@ -0,0 +1,46 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import chevronDown from "@/assets/icons/chevronDownIcon.svg";
+export default function LanguagesSwitcher() {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("English");
+  const languages = ["English", "French", "Spanish"];
+
+  const toggleDropdown = () => setOpen(!open);
+
+  const handleSelect = (lang: string) => {
+    setSelected(lang);
+    setOpen(false);
+  };
+
+  return (
+    <div className="relative w-[166px] h-[56px]">
+      <button
+        onClick={toggleDropdown}
+        className="w-full h-[56px] rounded-full flex justify-center gap-1.5 items-center border bg-[#BDBDBD]/20 px-4 "
+      >
+        <span className="text-[#BDBDBD] leading-[1.43]">{selected}</span>
+        <span className="flex justify-center items-center mt-0.5">
+          <Image src={chevronDown} alt="icon" />
+        </span>
+      </button>
+
+      {open && (
+        <ul className="absolute mt-2 w-full bg-white border border-gray-200 rounded-xl shadow z-10 overflow-hidden">
+          {languages.map((lang) => (
+            <li
+              key={lang}
+              onClick={() => handleSelect(lang)}
+              className={`px-4 py-2 cursor-pointer hover:bg-gray-100 ${
+                selected === lang ? "bg-gray-50 font-medium" : ""
+              }`}
+            >
+              {lang}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- components/layout/header/navbar.tsx
+++ components/layout/header/navbar.tsx
@@ -1,11 +1,13 @@
 import ToggleSwitch from "@/components/shared/switcher";
 import SearchInput from "./search-input";
+import LanguagesSwitcher from "./languages-switcher";
 
 export default function Navbar() {
   return (
     <header className="w-full py-2 h-[94px] bg-white px-[35px] flex justify-between items-center">
       <SearchInput />
       <ToggleSwitch />
+      <LanguagesSwitcher />
     </header>
   );
 }
